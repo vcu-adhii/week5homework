2833d9e790f522ffe6d26cc19f73cf7c52b2d2d5
Ibadadhi <72210975+Ibadadhi@users.noreply.github.com>
2021-03-07 17:29:04 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -1,21 +1,25 @@
 """Homework file for my students to have fun with some algorithms! """
 
+
 def find_greatest_number(incoming_list):
-    incoming_list = ([1, 2, 3, 4, 5, 6, 7, 8])
+    incoming_list = [1, 2, 3, 4, 5, 6, 7, 8]
     (max(incoming_list))
     pass
 
+
 def find_least_number(incoming_list):
-    incoming_list = ([1, 2, 3, 4, 5, 6, 7, 8])
+    incoming_list = [1, 2, 3, 4, 5, 6, 7, 8]
     (min(incoming_list))
     pass
 
+
 def add_list_numbers(incoming_list):
-    incoming_list = ([1, 2, 3, 4, 5, 6, 7, 8])
+    incoming_list = [1, 2, 3, 4, 5, 6, 7, 8]
     sum(incoming_list)
     pass
 
+
 def longest_value_key(incoming_dict):
-    incoming_dict = ({"dog": "cat", "a": "asdfasdfasdfasdfasdf"})
+    incoming_dict = {"dog": "cat", "a": "asdfasdfasdfasdfasdf"}
     max(incoming_dict, key=len)
     pass
