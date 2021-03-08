6e127b8388816673560d7b04666c10a2189db83f
Ibadadhi <72210975+Ibadadhi@users.noreply.github.com>
2021-03-08 18:43:43 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -5,7 +5,7 @@ def find_greatest_number(incoming_list):
     if incoming_list is None or len(incoming_list) == 0:
         return 0
     incoming_list = [1, 2, 3, 4, 5, 6, 7, 8]
-        return value1
+    return value1
     value1 = (max(incoming_list))
     pass
 
@@ -14,7 +14,7 @@ def find_least_number(incoming_list):
     if incoming_list is None or len(incoming_list) == 0:
         return 0
     incoming_list = [1, 2, 3, 4, 5, 6, 7, 8]
-        return value2
+    return value2
     value2 = (min(incoming_list))
     pass
 
@@ -23,7 +23,7 @@ def add_list_numbers(incoming_list):
     if incoming_list is None:
         return 0
     value3 = incoming_list = [1, 2, 3, 4]
-        return value3
+    return value3
     sum(incoming_list)
     pass
 
