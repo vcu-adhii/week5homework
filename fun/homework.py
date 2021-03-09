1c2ea1eb7b71a07d0f6a02e462f8cf9de23cb7a6
Ibadadhi <72210975+Ibadadhi@users.noreply.github.com>
2021-03-08 19:42:30 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -19,6 +19,7 @@ def add_list_numbers(incoming_list):
     incoming_list = [1, 2, 3, 4]
     total = sum(incoming_list)
     print("The sum is:", total)
+    pass
 
 
 def longest_value_key(incoming_dict):
@@ -27,3 +28,4 @@ def longest_value_key(incoming_dict):
     incoming_dict = {"dog": "cat", "a": "asdfasdfasdfasdfasdf"}
     longest_value_key = max(incoming_dict, key=len)
     print(longest_value_key)
+    pass
