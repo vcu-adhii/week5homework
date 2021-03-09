177f9d610af133146aea9e07886cdf67d2e5334a
Ibadadhi <72210975+Ibadadhi@users.noreply.github.com>
2021-03-08 19:35:20 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -16,8 +16,6 @@ def find_least_number(incoming_list):
 
 
 def add_list_numbers(incoming_list):
-    if incoming_list is None:
-        return 0
     incoming_list = [1, 2, 3, 4]
     total = sum(incoming_list)
     print("The sum is:",total)
@@ -26,12 +24,6 @@ def add_list_numbers(incoming_list):
 def longest_value_key(incoming_dict):
     if incoming_dict is None:
         return None
-    current_values_used = 0
-    longest_key = ""
-    for x, y in incoming_dict.values():
-        if len(y) > current_values_used:
-            current_values_used = len(y)
-            longest_key = y
-    for x, y in incoming_dict.values():
-        if y == longest_key:
-            return x
+    incoming_dict = {"dog": "cat", "a": "asdfasdfasdfasdfasdf"}
+    longest_key = max(incoming_dict, key=len)
+    print(longest_key)
