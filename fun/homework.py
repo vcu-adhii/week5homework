d9b050c45acf3a45a01405df14a68249352cea68
Ibadadhi <72210975+Ibadadhi@users.noreply.github.com>
2021-03-08 20:59:00 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -4,23 +4,21 @@
 def find_greatest_number(incoming_list):
     if incoming_list is None or len(incoming_list) == 0:
         return 0
-        return (max(incoming_list))
+    return (max(incoming_list))
     pass
 
 
 def find_least_number(incoming_list):
     if incoming_list is None or len(incoming_list) == 0:
         return 0
-        return (min(incoming_list))
+    return (min(incoming_list))
     pass
 
 
 def add_list_numbers(incoming_list):
-    total = 0
-    incoming_list = [1, 2, 3, 4]
-    for ele in range(0, len(incoming_list)):
-        total = total + incoming_list[ele]
-    print("Sum of all elements in given list: ", total)
+incoming_list = [1, 2, 3, 4]
+total = sum(incoming_list)
+print("The sum of this list:", total)
 
 
 def longest_value_key(incoming_dict):
