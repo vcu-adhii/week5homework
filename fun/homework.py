2c4b0c96968b38faf4a5eced8ee36c4194c4e902
Ibadadhi <72210975+Ibadadhi@users.noreply.github.com>
2021-03-08 20:37:30 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -18,7 +18,7 @@ def find_least_number(incoming_list):
 def add_list_numbers(incoming_list):
     incoming_list = [1, 2, 3, 4]
     total = sum(incoming_list)
-    return
+    return total
 
 def longest_value_key(incoming_dict):
     if incoming_dict is None:
